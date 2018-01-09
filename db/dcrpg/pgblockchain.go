7640f4b3c3c565be54cf7d5cd564b098ec80f180
Jon Chappelow <jonathan.chappelow@gmail.com>
2018-01-08 18:00:58 -0800

    Revert edits with Expires/ExpiresRevoked because of misunderstanding about stake.Node.

--- db/dcrpg/pgblockchain.go
+++ db/dcrpg/pgblockchain.go
@@ -711,7 +711,6 @@ func (pgb *ChainDB) StoreBlock(msgBlock *wire.MsgBlock, winningTickets []string,
 			return
 		}
 		winners = tpi.Winners
-		// can also access tpi.Expires
 	}
 
 	// Wrap the message block
